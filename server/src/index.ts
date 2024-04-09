180c5e2f5e186aab7e7f0bb988117257d4a1b6af
kawrou <121672581+kawrou@users.noreply.github.com>
2024-04-09 17:27:15 +0100

    Setup sockets server

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,1 +1,30 @@
-import express from 'express';
+import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
+import cors from "cors";
+
+const app = express();
+
+const server = createServer(app);
+app.use(cors());
+
+// const PORT = process.env.PORT || 3000;
+
+const io = new Server(server, {
+ cors: {
+  origin: "http://localhost:3000",
+  methods: ["GET", "POST"],
+ },
+});
+
+io.on("connection", (socket) => {
+ console.log(`A user connected: ${socket.id}`);
+
+ socket.on("disconnect", () => {
+  console.log(`User ${socket.id} disconnted`);
+ });
+});
+
+server.listen(3001, () => {
+ console.log("listening on *:3001");
+});
