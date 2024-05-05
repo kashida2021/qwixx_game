40ce417c5ab85447644737636c0b8051785479d1
kawrou <121672581+kawrou@users.noreply.github.com>
2024-05-06 01:33:11 +0900

    test: simple integration test of client/server interaction

--- server/src/index.test.ts
+++ server/src/index.test.ts
@@ -1,19 +1,49 @@
 import request from "supertest";
 import { app, server, io } from "./index";
+import { io as ioc, type Socket as ClientSocket } from "socket.io-client";
 
-describe("Express App", () => {
-    afterAll((done) => {
-      server.close(done);
-    });
-    
-    it("should start the server and respond with 404 for an unknown route", async () => {
-        const response = await request(app).get("/")
-        expect(response.status).toBe(404);
-    })
+describe.skip("Express App", () => {
+ afterAll((done) => {
+  server.close(done);
+ });
 
-    it.skip("responds with 200 for GET /", async () => {
-      const response = await request(app).get("/");
-      expect(response.status).toBe(200);
-    });
+ it("should start the server and respond with 404 for an unknown route", async () => {
+  const response = await request(app).get("/");
+  expect(response.status).toBe(404);
+ });
 
-  });
+ it.skip("responds with 200 for GET /", async () => {
+  const response = await request(app).get("/");
+  expect(response.status).toBe(200);
+ });
+});
+
+
+describe("socket events", () => {
+let clientSocket: ClientSocket; 
+
+beforeEach((done) => {
+  clientSocket = ioc("http://localhost:3001"); 
+  clientSocket.on('connect', () => {
+    done(); 
+  })
+})
+
+ afterEach((done) => {
+  io.close();
+  clientSocket.disconnect();
+  done()
+ });
+
+ it("should handle 'join_room' event", (done) => {
+  clientSocket.emit("join_room", "1");
+
+  clientSocket.on("join_room_success", ()=> {
+   const room = io.sockets.adapter.rooms.get("1");
+   console.log(room)
+   expect(room).toBeDefined();
+   expect(room?.has(clientSocket.id ?? "")).toBe(true);
+   done();
+  }); 
+ });
+});
--- server/src/index.ts
+++ server/src/index.ts
@@ -3,9 +3,9 @@ import { createServer } from "node:http";
 import { Server } from "socket.io";
 import cors from "cors";
 
-const app = express();
+const app = express(); // This creates an express app.
 
-const server = createServer(app);
+const server = createServer(app); // This creates an instance of http.Server using our express app.
 app.use(cors());
 
 // const PORT = process.env.PORT || 3000;
@@ -13,6 +13,8 @@ app.use(cors());
 //For getting visibility on which sockets are in each room
 const roomSockets: Record<string, string[]> = {};
 
+// This initializes Socket.IO with the http.Server instance.
+// It allows it to listen to WebSocket events on the same port as our express server. 
 const io = new Server(server, {
  cors: {
   origin: "http://localhost:5173",
@@ -26,7 +28,7 @@ io.on("connection", (socket) => {
  socket.on("join_room", (data) => {
   socket.join(data);
   console.log(`Socket ${socket.id} connected to room ${data}`)
-
+  socket.emit("join_room_success", data)
   //Setting data inside the roomSockets object above for visibility on what sockets are in what room
   //Can delete later if necessary
   if (!roomSockets[data]) {
@@ -38,9 +40,9 @@ io.on("connection", (socket) => {
   console.log(roomSockets)
  });
 
- socket.on("disconnect", () => {
-  console.log(`User ${socket.id} disconnected`);
- });
+//  socket.on("disconnect", () => {
+//   console.log(`User ${socket.id} disconnected`);
+//  });
 });
 
 server.listen(3001, () => {
