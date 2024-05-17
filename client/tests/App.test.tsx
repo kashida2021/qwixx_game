c1effbcb053bc5745db5f9188fb282298bb38378
kawrou <121672581+kawrou@users.noreply.github.com>
2024-05-17 14:25:00 +0100

    Added test case for a feature

--- client/tests/App.test.tsx
+++ client/tests/App.test.tsx
@@ -5,6 +5,7 @@ import { userEvent } from "@testing-library/user-event";
 import React from "react";
 import App from "../src/App.tsx";
 
+const user = userEvent.setup(); 
 // Tests
 describe("Renders main page correctly", () => {
  it("Should render the page correctly", () => {
@@ -20,4 +21,16 @@ describe("Renders main page correctly", () => {
   expect(input).toBeVisible(); 
   expect(btn).toBeVisible(); 
  });
+
+ 
+ test("when a user successfully joins a room",() => {
+    render(<App />); 
+    const input = screen.getByRole("textbox", {name: ""}); 
+    const btn = screen.getByRole("button", {name: "Join Room"}); 
+    user.type(input, "1"); 
+    user.click(btn); 
+
+    const roomNum = screen.getByText("Room 1"); 
+    expect(roomNum).toBeVisible(); 
+ })
 });
--- client/tests/setup.ts
+++ client/tests/setup.ts
@@ -1,6 +1,6 @@
 import { afterEach } from "vitest";
 import { cleanup } from "@testing-library/react";
-import "@testing-library/jest-dom/vitest";
+import "@testing-library/jest-dom";
 
 // runs a clean after each test case (e.g. clearing jsdom)
 afterEach(() => {
