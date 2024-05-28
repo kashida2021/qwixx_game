1b979d832dfebf50c00e24e235f8c94b3825140a
kawrou <121672581+kawrou@users.noreply.github.com>
2024-05-28 20:31:56 +0100

    Fix: first test after Ken's pull

--- server/src/socketHandlers.ts
+++ server/src/socketHandlers.ts
@@ -3,14 +3,13 @@ import { generateUniqueRoomId } from "./roomUtils";
 
 export default function initializeSocketHandler(io: Server) {
  //Object tracks current Lobby for each socketId. Key is SocketId and value is roomId
- const roomSockets: {[key: string]: string} = {};
+ const roomSockets: { [key: string]: string } = {};
 
  //Object tracks all lobbies and connected users in an array of strings
- const lobbies: {[key: string]: string[]} = {};
+ const lobbies: { [key: string]: string[] } = {};
 
  // Object that maps each socket.id to corresponding userId - can access this when disconnects
- const userIdList: {[key: string]: string} = {};
- 
+ const userIdList: { [key: string]: string } = {};
 
  io.on("connection", (socket) => {
   console.log(`A user connected: ${socket.id}`);
@@ -24,113 +23,113 @@ export default function initializeSocketHandler(io: Server) {
 
   //function to filter room within lobby object to remove target Id
   const filterLobbyIds = (lobby: string[], userId: string): string[] => {
-    return lobby.filter(id => id !== userId);
+   return lobby.filter((id) => id !== userId);
   };
-  
-  // Loops through each lobby in lobbies object and then callback filter function 
-  const updateLobbies = (lobbies: {[key: string]: string[]}, userId: string): void => {
-    for(const roomId in lobbies){
-      if(lobbies.hasOwnProperty(roomId)){
-        lobbies[roomId] = filterLobbyIds(lobbies[roomId], userId);
-      }
+
+  // Loops through each lobby in lobbies object and then callback filter function
+  const updateLobbies = (
+   lobbies: { [key: string]: string[] },
+   userId: string
+  ): void => {
+   for (const roomId in lobbies) {
+    if (lobbies.hasOwnProperty(roomId)) {
+     lobbies[roomId] = filterLobbyIds(lobbies[roomId], userId);
     }
+   }
   };
-  
-  socket.on("join_room", ({roomId, userId}) => {
-    //adds socket.id userId pair to userIdList object 
-    userIdList[socket.id] = userId;
 
-    if(!lobbies[roomId]){
-      lobbies[roomId] = [];
-    }
-
-    if (roomSockets[socket.id]) {
-      const currentLobby = roomSockets[socket.id];
-      socket.leave(currentLobby);
-      io.to(currentLobby).emit('userLeft', {userId: socket.id});
-    }
-
-
-    if(lobbies[roomId].length < 4){
-      lobbies[roomId].push(userId);
-      socket.join(roomId);
-      roomSockets[socket.id] = roomId;
-      io.to(roomId).emit('playerJoined', lobbies[roomId]);
-      console.log(roomSockets);
-      console.log(lobbies);
-    } else {
-      socket.emit("lobbyFull");
-    }
-  })
+  socket.on("join_room", ({ roomId, userId }) => {
+   //adds socket.id userId pair to userIdList object
+   console.log("join_room event received")
    
+   userIdList[socket.id] = userId;
 
-  // When a player explicity leaves a room - checks if roomId is already in roomSockets object. Then it will leave the currentRoom and also remove from roomSockets object. 
-   socket.on("leave_room", ({roomId, userId}) => {
-    if(roomSockets[socket.id] === roomId){
-      socket.leave(roomId);
-      console.log(`Socket ${userId} has left Room ${roomId}`)
-      delete roomSockets[socket.id];
-      delete userIdList[socket.id];
-    }
+   if (!lobbies[roomId]) {
+    lobbies[roomId] = [];
+   }
 
-    updateLobbies(lobbies, userId);
-
-    io.to(roomId).emit('userLeft', {userId: socket.id});
-
-    if(lobbies[roomId] && lobbies[roomId].length <= 0){
-      delete lobbies[roomId];
-    }
-
-    console.log(roomSockets);
-    console.log(lobbies);
-    console.log(userIdList);
-    
-      })
-
-   // Handles disconnect. If disconnected check to see if socketId is connected to a room. Will remove the socket from an existing room and delete from roomSockets object. 
-   socket.on('disconnect', () => {
+   if (roomSockets[socket.id]) {
     const currentLobby = roomSockets[socket.id];
-    updateLobbies(lobbies, userIdList[socket.id]);
-    
-    if(currentLobby) {
-      socket.leave(currentLobby);
-      console.log(`Socket ${socket.id} has left Room ${currentLobby}`)
-      io.to(currentLobby).emit('userLeft', {userId: socket.id});
-      delete roomSockets[socket.id];
-      delete userIdList[socket.id];
-    }
+    socket.leave(currentLobby);
+    io.to(currentLobby).emit("user_left", { userId: socket.id });
+   }
 
-      if(lobbies[currentLobby] && lobbies[currentLobby].length <= 0){
-        delete lobbies[currentLobby];
-      }
-
-      console.log(roomSockets);
-      console.log(lobbies);
-      console.log(userIdList);
-      })
+   if (lobbies[roomId].length < 4) {
+    lobbies[roomId].push(userId);
+    socket.join(roomId);
+    roomSockets[socket.id] = roomId;
+    io.to(roomId).emit("player_joined", lobbies[roomId]);
+    // console.log(roomSockets);
+    // console.log(lobbies);
+   } else {
+    socket.emit("lobbyFull");
+   }
   });
 
+  // When a player explicity leaves a room - checks if roomId is already in roomSockets object. Then it will leave the currentRoom and also remove from roomSockets object.
+  socket.on("leave_room", ({ roomId, userId }) => {
+   if (roomSockets[socket.id] === roomId) {
+    socket.leave(roomId);
+    // console.log(`Socket ${userId} has left Room ${roomId}`);
+    delete roomSockets[socket.id];
+    delete userIdList[socket.id];
+   }
+
+   updateLobbies(lobbies, userId);
+
+   io.to(roomId).emit("user_left", { userId: socket.id });
+
+   if (lobbies[roomId] && lobbies[roomId].length <= 0) {
+    delete lobbies[roomId];
+   }
+
+  //  console.log(roomSockets);
+  //  console.log(lobbies);
+  //  console.log(userIdList);
+  });
+
+  // Handles disconnect. If disconnected check to see if socketId is connected to a room. Will remove the socket from an existing room and delete from roomSockets object.
+  socket.on("disconnect", () => {
+   const currentLobby = roomSockets[socket.id];
+   updateLobbies(lobbies, userIdList[socket.id]);
+
+   if (currentLobby) {
+    socket.leave(currentLobby);
+    // console.log(`Socket ${socket.id} has left Room ${currentLobby}`);
+    io.to(currentLobby).emit("user_left", { userId: socket.id });
+    delete roomSockets[socket.id];
+    delete userIdList[socket.id];
+   }
+
+   if (lobbies[currentLobby] && lobbies[currentLobby].length <= 0) {
+    delete lobbies[currentLobby];
+   }
+
+  //  console.log(roomSockets);
+  //  console.log(lobbies);
+  //  console.log(userIdList);
+  });
 
   socket.on("create_lobby", () => {
-    const rooms = Array.from(io.sockets.adapter.rooms).map(
-     ([roomName, sockets]) => roomName
-    );
-    let room = generateUniqueRoomId(rooms);
- 
-    //Returns all the rooms a socket is currently in.
-    //  let socketRoomsArray = Array.from(socket.rooms.values()).filter((room) => room !==socket.id);
-    let socketRoomsArray = Array.from(socket.rooms);
- 
-    //Makes sures a socket can only ever be in one room
-    if (socket.rooms.size === 2) {
-     socket.leave(socketRoomsArray[1]);
-     socket.join(room);
-    }
- 
+   const rooms = Array.from(io.sockets.adapter.rooms).map(
+    ([roomName, sockets]) => roomName
+   );
+   let room = generateUniqueRoomId(rooms);
+
+   //Returns all the rooms a socket is currently in.
+   //  let socketRoomsArray = Array.from(socket.rooms.values()).filter((room) => room !==socket.id);
+   let socketRoomsArray = Array.from(socket.rooms);
+
+   //Makes sures a socket can only ever be in one room
+   if (socket.rooms.size === 2) {
+    socket.leave(socketRoomsArray[1]);
     socket.join(room);
-    socket.emit("create_lobby_success", room);
-    console.log(`Server: create_lobby_success: ${room}`);
-    console.log(`Client socket is in these rooms:${Array.from(socket.rooms)}`);
-   });
+   }
+
+   socket.join(room);
+   socket.emit("create_lobby_success", room);
+   console.log(`Server: create_lobby_success: ${room}`);
+   console.log(`Client socket is in rooms:${Array.from(socket.rooms)}`);
+  });
+ });
 }
-    
--- server/src/socketHandlers.unit.test.ts
+++ server/src/socketHandlers.unit.test.ts
@@ -5,6 +5,7 @@ import { type AddressInfo } from "node:net";
 import { io as ioc, type Socket as ClientSocket } from "socket.io-client";
 import { Server, type Socket as ServerSocket } from "socket.io";
 import initializeSocketHandler from "./socketHandlers";
+import { generateUniqueRoomId } from "./roomUtils";
 
 function waitFor(socket: ServerSocket | ClientSocket, event: string) {
  return new Promise((resolve) => {
@@ -12,62 +13,67 @@ function waitFor(socket: ServerSocket | ClientSocket, event: string) {
  });
 }
 
+jest.mock("./roomUtils", () => {
+ const generateUniqueRoomId = jest.fn().mockReturnValue("1234");
+ return { generateUniqueRoomId };
+});
+
 describe("socket event handler test", () => {
  let io: Server,
   serverSocket: ServerSocket,
   clientSocket1: ClientSocket,
   clientSocket2: ClientSocket;
 
- describe("socket event handlers with 1 client", () => {
-  beforeEach((done) => {
-   const httpServer = createServer();
-   io = new Server(httpServer);
-   httpServer.listen(() => {
-    const port = (httpServer.address() as AddressInfo).port;
-    clientSocket1 = ioc(`http://localhost:${port}`);
-    // Instead of using "io.on()" as shown in socket.io documentation above,
-    // pass the server instance to our imported "initalizeSocketHandler" so that we can test our event handlers
-    initializeSocketHandler(io);
-    clientSocket1.on("connect", () => {
-     done();
-    });
-   });
-  });
+ //  describe("socket event handlers with 1 client", () => {
+ //   beforeEach((done) => {
+ //    const httpServer = createServer();
+ //    io = new Server(httpServer);
+ //    httpServer.listen(() => {
+ //     const port = (httpServer.address() as AddressInfo).port;
+ //     clientSocket1 = ioc(`http://localhost:${port}`);
+ //     // Instead of using "io.on()" as shown in socket.io documentation above,
+ //     // pass the server instance to our imported "initalizeSocketHandler" so that we can test our event handlers
+ //     initializeSocketHandler(io);
+ //     clientSocket1.on("connect", () => {
+ //      done();
+ //     });
+ //    });
+ //   });
 
-  afterEach((done) => {
-   io.close();
-   clientSocket1.disconnect();
-   done();
-  });
+ //   afterEach((done) => {
+ //    io.close();
+ //    clientSocket1.disconnect();
+ //    done();
+ //   });
 
-  test("client socket can create a room", (done) => {
-   clientSocket1.emit("create_lobby");
+ //   test("client socket can create a room", (done) => {
+ //    clientSocket1.emit("create_lobby");
 
-   clientSocket1.on("create_lobby_success", () => {
-    const rooms = io.sockets.adapter.rooms.size;
-    expect(rooms).toEqual(2);
-    done();
-   });
-  });
+ //    clientSocket1.on("create_lobby_success", () => {
+ //     const rooms = io.sockets.adapter.rooms.size;
+ //     expect(rooms).toEqual(2);
+ //     done();
+ //    });
+ //   });
 
-  test("client can only create a single room", (done) => {
-   let emitEventsCount = 0;
+ //   test("client can only create a single room", (done) => {
+ //    let emitEventsCount = 0;
 
-   const checkRoomQuantity = () => {
-    emitEventsCount++;
+ //    const checkRoomQuantity = () => {
+ //     emitEventsCount++;
 
-    if (emitEventsCount === 2) {
-     const rooms = io.sockets.adapter.rooms.size;
-     expect(rooms).toEqual(2);
-     done();
-    }
-   };
-   //   console.log(`clientSocket1_id: ${clientSocket1.id}`)
-   clientSocket1.emit("create_lobby");
-   clientSocket1.emit("create_lobby");
-   clientSocket1.on("create_lobby_success", checkRoomQuantity);
-  });
- });
+ //     if (emitEventsCount === 2) {
+ //      const rooms = io.sockets.adapter.rooms.size;
+ //      expect(rooms).toEqual(2);
+ //      done();
+ //     }
+ //    };
+ //    //   console.log(`clientSocket1_id: ${clientSocket1.id}`)
+ //    clientSocket1.emit("create_lobby");
+ //    clientSocket1.emit("create_lobby");
+ //    clientSocket1.on("create_lobby_success", checkRoomQuantity);
+ //   });
+ //  });
 
  describe("socket event handlers with 2 clients", () => {
   beforeEach((done) => {
@@ -94,27 +100,24 @@ describe("socket event handler test", () => {
    clientSocket2.disconnect();
    done();
   });
-  //Probably need to somehow mock the generateRoomId function. 
-  test.todo("client sockets can't create the same room")
+  //Probably need to somehow mock the generateRoomId function.
+  test.todo("client sockets can't create the same room");
 
-  test("client sockets can join same room", (done) => {
-   let clientsInRoom = 0;
-
+  test.only("client sockets can join same room", (done) => {
    const checkClientsInRoom = () => {
-    clientsInRoom++;
+    const room = io.sockets.adapter.rooms.get("1234");
 
-    if (clientsInRoom === 2) {
-     const room = io.sockets.adapter.rooms.get("1");
-     expect(room?.has(clientSocket1.id ?? "")).toBe(true);
-     expect(room?.has(clientSocket2.id ?? "")).toBe(true);
-     done();
-    }
+    expect(room?.has(clientSocket1.id ?? "")).toBe(true);
+    expect(room?.has(clientSocket2.id ?? "")).toBe(true);
+    done();
    };
 
-   clientSocket1.emit("join_room", "1");
-   clientSocket2.emit("join_room", "1");
-   clientSocket1.on("join_room_success", checkClientsInRoom);
-   clientSocket2.on("join_room_success", checkClientsInRoom);
+   clientSocket1.emit("create_lobby");
+   clientSocket1.on("create_lobby_success", () => {
+    clientSocket2.emit("join_room", { roomId: "1234", userId: "testUser" });
+   });
+
+   clientSocket2.on("player_joined", checkClientsInRoom);
   });
 
   //This test is fails as no code has been implemented to ensure clients are only ever in 1 room
@@ -137,33 +140,35 @@ describe("socket event handler test", () => {
 
    clientSocket1.emit("join_room", "3");
    clientSocket2.emit("join_room", "4");
-   clientSocket1.on("join_room_success", checkClientsInRooms);
-   clientSocket2.on("join_room_success", checkClientsInRooms);
+   clientSocket1.on("player_joined", checkClientsInRooms);
+   clientSocket2.on("player_joined", checkClientsInRooms);
   });
 
- test.todo("clients can leave a room");
- test.only("clients can only be in one room at a time", (done) => {
-    let clientsInRoom = 0;
+  test.todo("clients can leave a room");
 
-  const checkClientsInRooms = () => {
-   clientsInRoom++;
-   if (clientsInRoom === 5) {
-    const room3 = io.sockets.adapter.rooms.get("3");
-    const room6 = io.sockets.adapter.rooms.get("6");
+  test("clients can only be in one room at a time", (done) => {
+   let clientsInRoom = 0;
 
-    expect(room3?.has(clientSocket1.id ?? "")).toBe(true);
-    expect(room3?.has(clientSocket2.id ?? "")).toBe(false);
-    expect(room6?.has(clientSocket1.id ?? "")).toBe(false);
-    expect(room6?.has(clientSocket2.id ?? "")).toBe(true);
-    done();
-   }
-  };
+   const checkClientsInRooms = () => {
+    clientsInRoom++;
+    if (clientsInRoom === 5) {
+     const room3 = io.sockets.adapter.rooms.get("3");
+     const room6 = io.sockets.adapter.rooms.get("6");
 
-  clientSocket1.emit("join_room", "3");
-  clientSocket2.emit("join_room", "3");
-  clientSocket2.emit("join_room", "6");
-  clientSocket1.on("join_room_success", checkClientsInRooms);
-  clientSocket2.on("join_room_success", checkClientsInRooms);
-  clientSocket2.on("join_room_success", checkClientsInRooms);
+     expect(room3?.has(clientSocket1.id ?? "")).toBe(true);
+     expect(room3?.has(clientSocket2.id ?? "")).toBe(false);
+     expect(room6?.has(clientSocket1.id ?? "")).toBe(false);
+     expect(room6?.has(clientSocket2.id ?? "")).toBe(true);
+     done();
+    }
+   };
+
+   clientSocket1.emit("join_room", "3");
+   clientSocket2.emit("join_room", "3");
+   clientSocket2.emit("join_room", "6");
+   clientSocket1.on("player_joined", checkClientsInRooms);
+   clientSocket2.on("player_joined", checkClientsInRooms);
+   clientSocket2.on("player_joined", checkClientsInRooms);
+  });
  });
 });
