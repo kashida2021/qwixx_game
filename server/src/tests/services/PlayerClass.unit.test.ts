974a148d6e82fadfdae33d20736483f6dbce892d
kawrou <121672581+kawrou@users.noreply.github.com>
2024-06-17 13:17:35 +0100

    TDD Player Class. Used getters for quick prototyping

---
+++ server/src/tests/services/PlayerClass.unit.test.ts
@@ -0,0 +1,19 @@
+import Player from "../../services/PlayerClass";
+
+const mockScoreCard = {
+  redRow: [1, 3, 5, 6],
+};
+
+describe("Player Class tests", () => {
+  it("Should take a name and be able to return it", () => {
+    const testPlayer = new Player("testPlayer", mockScoreCard);
+
+    expect(testPlayer.name).toEqual("testPlayer");
+  });
+
+  it("Should take a score card in its constructor and be able to return it", () => {
+    const testPlayer = new Player("testPlayer", mockScoreCard);
+
+    expect(testPlayer.scoreCard).toEqual(mockScoreCard);
+  });
+});
