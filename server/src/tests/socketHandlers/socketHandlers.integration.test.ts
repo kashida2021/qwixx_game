3108fd4c68fb7806a572f8129d4bf51fb432112c
kawrou <121672581+kawrou@users.noreply.github.com>
2024-06-19 16:33:20 +0100

    Started refactoring socketHandlers for better legibility and concerns

--- server/src/tests/socketHandlers/socketHandlers.integration.test.ts
+++ server/src/tests/socketHandlers/socketHandlers.integration.test.ts
@@ -7,59 +7,96 @@ import { Server, type Socket as ServerSocket } from "socket.io";
 import initializeSocketHandler from "../../socketHandlers/socketHandlers";
 
 describe("socket event handler test", () => {
- let io: Server,
-  serverSocket: ServerSocket,
-  clientSocket1: ClientSocket,
-  clientSocket2: ClientSocket;
+  let io: Server,
+    serverSocket: ServerSocket,
+    clientSocket1: ClientSocket,
+    clientSocket2: ClientSocket;
 
- describe("socket event handlers with 1 client", () => {
-  beforeEach((done) => {
-   const httpServer = createServer();
-   io = new Server(httpServer);
-   httpServer.listen(() => {
-    const port = (httpServer.address() as AddressInfo).port;
-    clientSocket1 = ioc(`http://localhost:${port}`);
-    // Instead of using "io.on()" as shown in socket.io documentation above,
-    // pass the server instance to our imported "initalizeSocketHandler" so that we can test our event handlers
-    initializeSocketHandler(io);
-    clientSocket1.on("connect", () => {
-     done();
+  describe("socket event handlers with 1 client", () => {
+    beforeEach((done) => {
+      const httpServer = createServer();
+      io = new Server(httpServer);
+      httpServer.listen(() => {
+        const port = (httpServer.address() as AddressInfo).port;
+        clientSocket1 = ioc(`http://localhost:${port}`);
+        // Instead of using "io.on()" as shown in socket.io documentation above,
+        // pass the server instance to our imported "initalizeSocketHandler" so that we can test our event handlers
+        initializeSocketHandler(io);
+        clientSocket1.on("connect", () => {
+          done();
+        });
+      });
     });
-   });
+
+    afterEach((done) => {
+      io.close();
+      clientSocket1.disconnect();
+      done();
+    });
+
+    test("client socket can create a room", (done) => {
+      clientSocket1.emit("create_lobby", "clientSocket1", () => {
+        const rooms = io.sockets.adapter.rooms.size;
+        expect(rooms).toEqual(2);
+        done();
+      });
+    });
+
+    test("client can only create a single room", (done) => {
+      let emitEventsCount = 0;
+
+      const checkRoomQuantity = () => {
+        emitEventsCount++;
+
+        if (emitEventsCount === 2) {
+          const rooms = io.sockets.adapter.rooms.size;
+          expect(rooms).toEqual(2);
+          done();
+        }
+      };
+      clientSocket1.emit("create_lobby", "clientSocket1", () => {
+        checkRoomQuantity();
+      });
+      clientSocket1.emit("create_lobby", "clientSocket1", () => {
+        checkRoomQuantity();
+      });
+    });
   });
 
-  afterEach((done) => {
-   io.close();
-   clientSocket1.disconnect();
-   done();
+  describe.skip("socket event handlers with 2 clients", () => {
+    beforeEach((done) => {
+      const httpServer = createServer();
+      io = new Server(httpServer);
+      httpServer.listen(() => {
+        const port = (httpServer.address() as AddressInfo).port;
+        clientSocket1 = ioc(`http://localhost:${port}`);
+        initializeSocketHandler(io);
+        clientSocket1.on("connect", () => {
+          clientSocket2 = ioc(`http://localhost:${port}`);
+          clientSocket2.on("connect", () => {
+            done();
+          });
+        });
+      });
+    });
+    afterEach((done) => {
+      io.close();
+      clientSocket1.disconnect();
+      clientSocket2.disconnect();
+      done();
+    });
+
+    it.skip("Can start a game", (done) => {
+      clientSocket1.emit("create_lobby", "clientSocket1", () => {
+        clientSocket2.emit("join_lobby", {
+          localLobbyId: "1234",
+          userId: "clientSocket2",
+        }, () => {
+            console.log(io.sockets.adapter.rooms.get("1234")); 
+            done(); 
+        });
+      });
+
+    });
   });
-
-  test("client socket can create a room", (done) => {
-   clientSocket1.emit("create_lobby", "clientSocket1", () => {
-    const rooms = io.sockets.adapter.rooms.size;
-    expect(rooms).toEqual(2);
-    done();
-   });
-  });
-
-  test("client can only create a single room", (done) => {
-   let emitEventsCount = 0;
-
-   const checkRoomQuantity = () => {
-    emitEventsCount++;
-
-    if (emitEventsCount === 2) {
-     const rooms = io.sockets.adapter.rooms.size;
-     expect(rooms).toEqual(2);
-     done();
-    }
-   };
-   clientSocket1.emit("create_lobby", "clientSocket1", () => {
-    checkRoomQuantity(); 
-   });
-   clientSocket1.emit("create_lobby", "clientSocket1", () => {
-    checkRoomQuantity(); 
-   });
-  });
- });
 });
--- server/src/utils/roomUtils.ts
+++ server/src/utils/roomUtils.ts
@@ -1,7 +1,10 @@
+import { Socket } from "socket.io";
+
 //Uses a simple do/while loop to check the generated random id is unique
 //If there are too many rooms, I wonder if it'll take too long and
 //if there is some algorithm that would help with that
 export const generateUniqueRoomId = (rooms: string[]): string => {
+    console.log("generateUniqueRoomId was called")
  let room: string;
  do {
   room = Math.floor(1000 + Math.random() * 9000).toString();
@@ -9,3 +12,12 @@ export const generateUniqueRoomId = (rooms: string[]): string => {
  return room;
 };
 
+export const removeSocketFromRooms = (socket:Socket): void => {
+    console.log("removeSocketFromRooms was called")
+    let socketRoomsArray = Array.from(socket.rooms); 
+    if(socketRoomsArray.length > 1){
+        for(let i = 1; i < socketRoomsArray.length; i++){
+            socket.leave(socketRoomsArray[i]); 
+        }
+    }
+}
