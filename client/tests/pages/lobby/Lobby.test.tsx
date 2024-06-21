e1d4c44049f8ab370c6b55f68b2ee5bc0f7875c0
kawrou <121672581+kawrou@users.noreply.github.com>
2024-06-21 22:28:59 +0100

    Fixed Lobby test by passing missing props

--- client/tests/pages/lobby/Lobby.test.tsx
+++ client/tests/pages/lobby/Lobby.test.tsx
@@ -1,17 +1,32 @@
 import Lobby from "../../../src/pages/Lobby/Lobby";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import "@testing-library/jest-dom";
+import { MemoryRouter } from "react-router-dom";
+import { socket } from "../../../src/services/socketServices";
 
-const lobbyIdMock = "1234"
+const lobbyIdMock = "1234";
+const setMembersMock = vi.fn();
+const setNotificationsMock = vi.fn();
 
 describe("Lobby:", () => {
-    it("should render correct elements", () => {
-        render(<Lobby lobbyId = {lobbyIdMock}/>)
+  it("should render correct elements", () => {
+    render(
+      <MemoryRouter>
+        <Lobby
+          socket={socket}
+          lobbyId={lobbyIdMock}
+          userId={"test-user"}
+          members={["test-user"]}
+          setMembers={setMembersMock}
+          notifications={[""]}
+          setNotifications={setNotificationsMock}
+        />
+      </MemoryRouter>
+    );
 
-        const lobbyId = screen.getByText("1234");
-        expect(lobbyId).toBeVisible(); 
-
-    })
-})
+    const lobbyId = screen.getByText("Lobby: 1234");
+    expect(lobbyId).toBeVisible();
+  });
+});
