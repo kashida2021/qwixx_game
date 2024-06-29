31fcdea42973599fd2842f34c5dc56582ede4513
Ken Ashida <kenashida@hotmail.com>
2024-06-29 13:58:16 +0100

    Lobby test fix

--- client/tests/pages/lobby/Lobby.test.tsx
+++ client/tests/pages/lobby/Lobby.test.tsx
@@ -1,16 +1,25 @@
 import Lobby from "../../../src/pages/Lobby/Lobby";
-import { describe, it, expect } from "vitest";
+import { describe, it, expect, vi } from "vitest";
 import { render, screen } from "@testing-library/react";
 import React from "react";
 import "@testing-library/jest-dom";
+import { socket } from "../../../src/services/socketServices";
+import { MemoryRouter } from "react-router-dom";
 
 const lobbyIdMock = "1234"
+const userIdMock = "John"
+const setMembersMock = vi.fn();
+const setNotificationsMock = vi.fn();
 
 describe("Lobby:", () => {
     it("should render correct elements", () => {
-        render(<Lobby lobbyId = {lobbyIdMock}/>)
+        render(
+            <MemoryRouter>
+                <Lobby lobbyId = {lobbyIdMock} socket={socket} userId={userIdMock} members = {[]} setMembers = {setMembersMock} notifications = {[]} setNotifications={setNotificationsMock}/>
+            </MemoryRouter>
+        )
 
-        const lobbyId = screen.getByText("1234");
+        const lobbyId = screen.getByText(/1234/);
         expect(lobbyId).toBeVisible(); 
 
     })
