d691b791e19e235eff1b73c49d7746c2d9d85b8f
kawrou <121672581+kawrou@users.noreply.github.com>
2024-07-01 11:49:43 +0100

    Moved GameBoard class into models and integrated with Initializer

--- server/src/tests/models/InitializeScoreBoard.test.ts
+++ server/src/tests/models/InitializeScoreBoard.test.ts
@@ -1,8 +1,12 @@
 import { initializeScoreBoards } from "../../models/InitializeScoreBoard";
+import GameBoard from "../../models/GameBoardTemp";
 
 describe("initializeScoreBoards tests", () => {
   test("given 2 player names, it returns an array of 2 ScoreBoard objects", () => {
     const scoreBoardsArray = initializeScoreBoards(["player1", "player2"]);
+    scoreBoardsArray.forEach((scoreBoard) => {
+      expect(scoreBoard instanceof GameBoard).toBe(true); 
+    })
     expect(scoreBoardsArray.length).toBe(2);
   });
 
