75f76fe362423a6f02c708750a1525973a1fcd04
Ken Ashida <kenashida@hotmail.com>
2024-07-06 12:59:52 +0100

    feat/refactored code for generating rows and row cells

--- client/src/components/gameCard/row.tsx
+++ client/src/components/gameCard/row.tsx
@@ -1,27 +1,24 @@
 import React from 'react';
 import RowCell from './rowCell';
 
-interface RowNumber {
-    number: number;
-    crossed: boolean;
-}
-
 interface RowProps{
-    row: {
-        color: string;
-        numbers: RowNumber[];
-
-    };
+    rowColour: string; 
     rowIndex: number;
+    numbers: number;
 }
 
-const Row:React.FC<RowProps> = ({row, rowIndex}) => {
+const Row:React.FC<RowProps> = ({rowColour, rowIndex, numbers}) => {
+
+    const buttonNumbers = rowIndex < 2
+    ? Array.from({ length: numbers }, (_, i) => i + 2)
+    : Array.from({ length: numbers }, (_, i) => numbers + 1 - i);
+
     return (
-        <div className={`Row ${row.color}`}>
-            {row.numbers.map((num, numIndex) => (
-                <RowCell key={numIndex} type="number" color={row.color} numIndex={numIndex} num={num} rowIndex={rowIndex} />
+        <div className={`Row ${rowColour}`}>
+            {buttonNumbers.map((num, numIndex) => (
+                <RowCell key={numIndex} type="number" color={rowColour} numIndex={numIndex} num={num} rowIndex={rowIndex} />
             ))}
-        <RowCell type="lock" color={row.color}/>
+        <RowCell type="lock" color={rowColour}/>
         </div>
     )
 
