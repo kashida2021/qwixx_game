44819519f63289e04d6a0fe80d6317e2f418e216
Ken Ashida <kenashida@hotmail.com>
2024-08-04 09:19:40 +0100

    updated penalties box with checkboxes

--- client/src/components/gameCard/gameCard.tsx
+++ client/src/components/gameCard/gameCard.tsx
@@ -1,11 +1,21 @@
 import Row from './row';
 import "./GameCard.css";
+import {useState, ChangeEvent} from "react";
+
 
 const rowColours: string[] = ["red", "yellow", "green", "blue"];
 const numbers: number = 11;
 
 
 const GameCard = () => {
+
+    const [penalties, setPenalties] = useState<string[]>([]);
+
+    const handlePenaltyChange = (event: ChangeEvent<HTMLInputElement>) => {
+        const value = event.target.value;
+        setPenalties((prevPenalties) => [...prevPenalties, value]);
+        event.target.disabled = true;
+    }
 
     return (
         <div className="gameCard">
@@ -14,10 +24,44 @@ const GameCard = () => {
             ))}
             <div className="penalties">
                 <h3>Penalties</h3>
-                <button></button>
-                <button></button>
-                <button></button>
-                <button></button>
+                <ul>
+                    <li>
+                        <input
+                            type="checkbox"
+                            id="penalty1"
+                            value="Penalty_1"
+                            onChange={handlePenaltyChange}
+                        />
+                        <label htmlFor="penalty1">1</label>
+                    </li>
+                    <li>
+                    <input
+                            type="checkbox"
+                            id="penalty2"
+                            value="Penalty_2"
+                            onChange={handlePenaltyChange}
+                        />
+                        <label htmlFor="penalty2">2</label>
+                    </li>
+                    <li>
+                    <input
+                            type="checkbox"
+                            id="penalty3"
+                            value="Penalty_3"
+                            onChange={handlePenaltyChange}
+                        />
+                        <label htmlFor="penalty3">3</label>
+                    </li>
+                    <li>
+                    <input
+                            type="checkbox"
+                            id="penalty4"
+                            value="Penalty_4"
+                            onChange={handlePenaltyChange}
+                        />
+                        <label htmlFor="penalty1">4</label>
+                    </li>
+                </ul>
             </div>
         </div>
     )
