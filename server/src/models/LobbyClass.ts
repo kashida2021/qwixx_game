9306d5cd4045bbcc6ac46e28ae5328e63caeae36
Ken Ashida <kenashida@hotmail.com>
2024-08-11 19:13:12 +0100

    feat/refactored socketHandler to use LobbyClass. Added additional Lobby Class unit tests

--- server/src/models/LobbyClass.ts
+++ server/src/models/LobbyClass.ts
@@ -1,36 +1,34 @@
+export default class Lobby {
+  private _lobbyId: string;
+  private _players: string[];
 
-export default class Lobby{
-    private _lobbyId: string;
-    private _players: string[];
+  constructor(lobbyId: string) {
+    this._lobbyId = lobbyId;
+    this._players = [];
+  }
 
-    constructor(lobbyId: string){
-        this._lobbyId = lobbyId;
-        this._players = [];
+  get lobbyId(): string {
+    return this._lobbyId;
+  }
+
+  get players(): string[] {
+    return this._players;
+  }
+
+  addPlayer(userId: string): boolean {
+    if (this._players.length < 4) {
+      this._players.push(userId);
+      return true;
+    } else {
+      return false; // Lobby is full
     }
+  }
 
-    get lobbyId(): string {
-        return this._lobbyId;
-      }
-    
-    get players(): string[] {
-        return this._players;
-      }
+  removePlayer(userId: string): void {
+    this._players = this._players.filter((playerId) => playerId !== userId);
+  }
 
-    addPlayer(userId: string): boolean {
-        if (this._players.length < 4) {
-          this._players.push(userId);
-          return true;
-        } else {
-          return false; // Lobby is full
-        }
-      }
-
-    removePlayer(userId: string): void {
-        this._players = this._players.filter(playerId => playerId !== userId);
-      }
-    
-      isFull(): boolean {
-        return this._players.length >= 4;
-      }
-    
+  isFull(): boolean {
+    return this._players.length >= 4;
+  }
 }
