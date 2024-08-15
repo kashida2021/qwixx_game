6c01d2d70c926524d112a90ba2795421ffabde4c
kawrou <121672581+kawrou@users.noreply.github.com>
2024-08-15 22:42:49 +0100

    feat:Use enum and GameCardData type in GameCard.tsx

--- client/src/components/GameCard/GameCard.tsx
+++ client/src/components/GameCard/GameCard.tsx
@@ -1,24 +1,26 @@
 import Row from "./Row";
 import "./GameCard.css";
 import { useState, ChangeEvent } from "react";
+import { GameCardData } from "../../types/GameCardData";
+import { RowColour } from "../../types/enums";
 
-const rowColours: string[] = ["red", "yellow", "green", "blue"];
+const rowColours: RowColour[] = [RowColour.Red, RowColour.Yellow, RowColour.Green, RowColour.Blue];
 const numbers: number = 11;
 
 interface IGameCard {
   member: string;
   isOpponent: boolean;
-  gameCardData: any;
+  gameCardData: GameCardData;
 }
 
 const GameCard: React.FC<IGameCard> = ({ member, isOpponent, gameCardData }) => {
   //PLAYER ID ASSOCIATED TO EACH GAME CARD
   //That can be used along with row colour + number to send to server
-  const [penalties, setPenalties] = useState<string[]>([]);
+  // const [penalties, setPenalties] = useState<string[]>([]);
 
   const handlePenaltyChange = (event: ChangeEvent<HTMLInputElement>) => {
     const value = event.target.value;
-    setPenalties((prevPenalties) => [...prevPenalties, value]);
+    // setPenalties((prevPenalties) => [...prevPenalties, value]);
     event.target.disabled = true;
   };
 
