d5de4a949caf8bab5927e15e26fd42be7709bca8
kawrou <121672581+kawrou@users.noreply.github.com>
2024-08-16 11:19:48 +0100

    ref:Update locked rows att of GameCardData

--- client/src/types/GameCardData.ts
+++ client/src/types/GameCardData.ts
@@ -1,8 +1,13 @@
 export interface GameCardData {
-    red: number[],
-    yellow: number[],
-    green: number[],
-    blue: number[],
-    lockedRows: string[],
-    penalties: number,
+  red: number[];
+  yellow: number[];
+  green: number[];
+  blue: number[];
+  isLocked: {
+    red: boolean;
+    yellow: boolean;
+    green: boolean;
+    blue: boolean;
+  };
+  penalties: number;
 }
