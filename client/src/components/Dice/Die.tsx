7677c6c35f9d5a012fd08f809f9b63c40a9bb103
kawrou <121672581+kawrou@users.noreply.github.com>
2024-09-06 15:44:15 +0100

    ref:Added types

--- client/src/components/Dice/Die.tsx
+++ client/src/components/Dice/Die.tsx
@@ -1,31 +1,58 @@
 import "./Die.css";
 
-const Pip = ({ color }) => {
-  return <span aria-label="die pip" className="pip" style={{ backgroundColor: color }} />;
+interface IPipProps {
+  colour: string;
+}
+
+interface IFaceProps {
+  children: React.ReactNode;
+  backgroundColour: string;
+  colourKey: string;
+}
+
+interface IDieProps {
+  colour: string;
+  colourKey: string;
+  value: number;
+}
+
+const Pip: React.FC<IPipProps> = ({ colour }) => {
+  return (
+    <span
+      aria-label="die pip"
+      className="pip"
+      style={{ backgroundColor: colour }}
+    />
+  );
 };
 
-
-const Face = ({ children, backgroundColor, colourKey }) => (
-  <div aria-label={`${colourKey} die`}className={`face dice ${backgroundColor}`} >
+const Face: React.FC<IFaceProps> = ({
+  children,
+  backgroundColour,
+  colourKey,
+}) => (
+  <div
+    aria-label={`${colourKey} die`}
+    className={`face dice ${backgroundColour}`}
+  >
     {children}
   </div>
 );
 
-const Die: React.FC<{ colour: string; colourKey: string; value: number }> = ({
-  colour,
-  colourKey,
-  value,
-}) => {
-
-  const pipColour = colour === 'white' ? '#333' : '#fff';
+const Die: React.FC<IDieProps> = ({ colour, colourKey, value }) => {
+  const pipColour = colour === "white" ? "#333" : "#fff";
 
   const pips = Number.isInteger(value)
     ? Array(value)
         .fill(0)
-        .map((_, i) => <Pip key={i} color={pipColour} />)
+        .map((_, i) => <Pip key={i} colour={pipColour} />)
     : null;
-  
-  return <Face backgroundColor={colour} colourKey={colourKey}>{pips}</Face>;
+
+  return (
+    <Face backgroundColour={colour} colourKey={colourKey}>
+      {pips}
+    </Face>
+  );
 };
 
 export default Die;
