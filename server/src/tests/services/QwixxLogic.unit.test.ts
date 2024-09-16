97abf60cab8a0334a435f57569174c6afa3f9637
kawrou <121672581+kawrou@users.noreply.github.com>
2024-09-16 19:20:57 +0100

    ref:makeMove method and added private identifer to class methods

--- server/src/tests/services/QwixxLogic.unit.test.ts
+++ server/src/tests/services/QwixxLogic.unit.test.ts
@@ -11,13 +11,17 @@ const gameCardMock: Partial<qwixxBaseGameCard> = {
 const player1Mock: Partial<Player> = {
   name: "player1",
   gameCard: gameCardMock as qwixxBaseGameCard,
+  hasSubmittedChoice: false,
   serialize: jest.fn(),
+  markSubmitted: jest.fn(), 
 };
 
 const player2Mock: Partial<Player> = {
   name: "player2",
   gameCard: gameCardMock as qwixxBaseGameCard,
+  hasSubmittedChoice: false,
   serialize: jest.fn(),
+  markSubmitted: jest.fn(),
 };
 
 const diceMock: Partial<Dice> = {
@@ -30,26 +34,15 @@ const playersArrayMock: Player[] = [
 ];
 
 describe("Qwixx Logic tests", () => {
-  it.skip("should make a move and return the correct result", () => {
+  it("should make a move and return the correct result", () => {
     (gameCardMock.markNumbers! as jest.Mock).mockReturnValue(true);
 
     const testGame = new QwixxLogic(playersArrayMock, diceMock as Dice);
 
     const gameState = testGame.makeMove("player1", "red", 1);
-    console.log(gameState);
-    // expect(gameState).toEqual({
-    //   playerName: "player1",
-    //   row: "red",
-    //   num: 1,
-    // });
     expect(gameCardMock.markNumbers).toHaveBeenCalledWith("red", 1);
 
     const player2result = testGame.makeMove("player2", "blue", 1);
-    // expect(player2result).toEqual({
-    //   playerName: "player2",
-    //   row: "blue",
-    //   num: 1,
-    // });
     expect(gameCardMock.markNumbers).toHaveBeenCalledWith("blue", 1);
   });
 
@@ -58,4 +51,6 @@ describe("Qwixx Logic tests", () => {
     const result = testGame.makeMove("", "red", 1);
     expect(result).toBe("Player not found");
   });
+
+  test("haveAllPlayersSubmitted should be false ")
 });
