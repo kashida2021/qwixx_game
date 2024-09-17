4dcd66a7a1c5871b87cfd8e8dedeba57addf51d1
kawrou <121672581+kawrou@users.noreply.github.com>
2024-09-17 15:16:18 +0100

    test:markSubmitted() method in Player class

--- server/src/tests/models/PlayerClass.unit.test.ts
+++ server/src/tests/models/PlayerClass.unit.test.ts
@@ -6,25 +6,26 @@ const mockGameCard: Partial<qwixxBaseGameCard> = {
   markNumbers: jest.fn(),
 };
 
-describe("Player Class tests", () => {
-  it("Should take a name and be able to return it", () => {
-    const testPlayer = new Player("testPlayer", mockGameCard as qwixxBaseGameCard);
+let testPlayer: Player;
 
+describe("Player Class tests", () => {
+  beforeEach(() => {
+    testPlayer = new Player("testPlayer", mockGameCard as qwixxBaseGameCard);
+  });
+
+  it("Should take a name and be able to return it", () => {
     expect(testPlayer.name).toEqual("testPlayer");
   });
 
   test("submission count should increment when incrementSubmissionCount is called", () => {
     (mockGameCard.markNumbers! as jest.Mock).mockReturnValue(true);
    
-    const testPlayer = new Player("testPlayer", mockGameCard as qwixxBaseGameCard);
-    
     testPlayer.markNumber(rowColour.Red,2);
     expect(testPlayer.submissionCount).toBe(1);
   })
 
   test("submission count should return to 0 after markSubmitted() call", () => {
     (mockGameCard.markNumbers! as jest.Mock).mockReturnValue(true);
-    const testPlayer = new Player("testPlayer", mockGameCard as qwixxBaseGameCard);
 
     testPlayer.markNumber(rowColour.Red, 2);
     testPlayer.markNumber(rowColour.Red, 3);
@@ -32,5 +33,13 @@ describe("Player Class tests", () => {
 
     testPlayer.resetSubmission();
     expect(testPlayer.submissionCount).toBe(0);
+  })
+
+  test("markSubmitted should set hasSubmittedChoice state", () => {
+    expect(testPlayer.hasSubmittedChoice).toBeFalsy();
+
+    testPlayer.markSubmitted();
+
+    expect(testPlayer.hasSubmittedChoice).toBeTruthy();
   })
 });
