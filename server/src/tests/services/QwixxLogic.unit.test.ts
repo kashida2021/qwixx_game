163f8653e62c9afdfbf20b472e5cd2c1f9d68474
kawrou <121672581+kawrou@users.noreply.github.com>
2024-10-01 23:59:27 +0100

    test:Create separate test files to explore mocking methods

--- server/src/tests/services/QwixxLogic.unit.test.ts
+++ server/src/tests/services/QwixxLogic.unit.test.ts
@@ -5,85 +5,11 @@ import Dice from "../../models/DiceClass";
 import { DiceColour } from "../../enums/DiceColours";
 import SixSidedDie from "../../models/SixSidedDieClass";
 
-// const gameCardMock: Partial<qwixxBaseGameCard> = {
-//   markNumbers: jest.fn(),
-// };
-
-// const player1Mock: Partial<Player> = {
-//   name: "player1",
-//   // gameCard: gameCardMock as qwixxBaseGameCard,
-//   hasSubmittedChoice: false,
-//   serialize: jest.fn(),
-//   markSubmitted: jest.fn(),
-//   markNumber: jest.fn(),
-// };
-
-// const player2Mock: Partial<Player> = {
-//   name: "player2",
-//   // gameCard: gameCardMock as qwixxBaseGameCard,
-//   hasSubmittedChoice: false,
-//   serialize: jest.fn(),
-//   markSubmitted: jest.fn(),
-//   markNumber: jest.fn(),
-// };
-
-// const diceMock: Partial<Dice> = {
-//   diceValues: {
-//     [DiceColour.White1]: 5,
-//     [DiceColour.White2]: 5,
-//     [DiceColour.Red]: 5,
-//     [DiceColour.Yellow]: 5,
-//     [DiceColour.Green]: 5,
-//     [DiceColour.Blue]: 5,
-//   },
-//   rollAllDice: jest.fn(),
-//   serialize: jest.fn(),
-// };
-
-// const playersArrayMock: Player[] = [
-//   player1Mock as Player,
-//   player2Mock as Player,
-// ];
-
 // Need to instantiate QwixxLogic class
 // I can:
 //create a fake
 //spyOn the method
 //mock a function?
-
-/**
- * Using inheritance to create mock
- */
-// class qwixxBaseGameCardMock extends qwixxBaseGameCard {
-//   markNumbers = jest.fn();
-// }
-// class playerMock extends Player {
-//   markNumber = jest.fn();
-// }
-
-// class diceMock extends Dice {
-//   get diceValues(): Record<DiceColour, number> {
-//     return {
-//       white1: 5,
-//       white2: 5,
-//       red: 5,
-//       yellow: 5,
-//       green: 5,
-//       blue: 5,
-//     };
-//   }
-//   rollAllDice = jest.fn();
-//   serialize = jest.fn();
-// }
-
-// class SixSidedDieMock extends SixSidedDie {}
-
-// const fakeDice = new diceMock(SixSidedDieMock);
-// const gameCardMock1 = new qwixxBaseGameCardMock();
-// const gameCardMock2 = new qwixxBaseGameCardMock();
-// const player1Mock = new playerMock("player1", gameCardMock1);
-// const player2Mock = new playerMock("player2", gameCardMock2);
-// const playersArrayMock = [player1Mock, player2Mock];
 
 const MockedGameCardClass = qwixxBaseGameCard as jest.Mocked<
   typeof qwixxBaseGameCard
@@ -91,45 +17,40 @@ const MockedGameCardClass = qwixxBaseGameCard as jest.Mocked<
 const gameCardMock1 = new MockedGameCardClass();
 const gameCardMock2 = new MockedGameCardClass();
 
-const player1Mock = new Player('player1', gameCardMock1) as jest.Mocked<Player>;
+const player1Mock = new Player("player1", gameCardMock1) as jest.Mocked<Player>;
 player1Mock.markNumber = jest.fn();
-(player1Mock.markNumber as jest.Mock).mockReturnValue(true);
+player1Mock.markNumber.mockReturnValue(true);
 
-const player2Mock = new Player('player2', gameCardMock1) as jest.Mocked<Player>;
+const player2Mock = new Player("player2", gameCardMock2) as jest.Mocked<Player>;
 player2Mock.markNumber = jest.fn();
-(player2Mock.markNumber as jest.Mock).mockReturnValue(true);
+player2Mock.markNumber.mockReturnValue(true);
 
 const MockedDie = SixSidedDie as jest.Mocked<typeof SixSidedDie>;
 const MockedDiceClass = Dice as jest.Mocked<typeof Dice>;
 const fakeDice = new MockedDiceClass(MockedDie);
 jest.spyOn(fakeDice, "diceValues", "get").mockReturnValue({
-    [DiceColour.White1]: 5,
-    [DiceColour.White2]: 5,
-    [DiceColour.Red]: 5,
-    [DiceColour.Yellow]: 5,
-    [DiceColour.Green]: 5,
-    [DiceColour.Blue]: 5,
+  [DiceColour.White1]: 5,
+  [DiceColour.White2]: 5,
+  [DiceColour.Red]: 5,
+  [DiceColour.Yellow]: 5,
+  [DiceColour.Green]: 5,
+  [DiceColour.Blue]: 5,
 });
 jest.spyOn(fakeDice, "validColouredNumbers", "get").mockReturnValue({
-    [DiceColour.Red]: [10, 10],
-    [DiceColour.Yellow]: [10, 10],
-    [DiceColour.Green]: [10, 10],
-    [DiceColour.Blue]: [10, 10],
+  [DiceColour.Red]: [10, 10],
+  [DiceColour.Yellow]: [10, 10],
+  [DiceColour.Green]: [10, 10],
+  [DiceColour.Blue]: [10, 10],
 });
 
 const playersArrayMock = [player1Mock, player2Mock];
 
 describe("Qwixx Logic tests", () => {
   beforeEach(() => {
-    jest.clearAllMocks(); 
+    jest.clearAllMocks();
   });
 
   it("should call the markNumber method with correct args", () => {
-    // (player1Mock.markNumber as jest.Mock).mockReturnValue(true);
-
-    // Only works when extending Player class
-    // player1Mock.markNumber.mockReturnValue(true);
-    // const testGame = new QwixxLogic(playersArrayMock, diceMock as Dice);
     const testGame = new QwixxLogic(playersArrayMock, fakeDice);
     testGame.rollDice();
     testGame.makeMove("player1", "red", 10);
@@ -158,8 +79,6 @@ describe("Qwixx Logic tests", () => {
   });
 
   test("non-active player can mark a number equal to the sum of the white dice", () => {
-    // (player2Mock.markNumber as jest.Mock).mockReturnValue(true);
-    // player2Mock.markNumber.mockReturnValue(true);
     const testGame = new QwixxLogic(playersArrayMock, fakeDice);
 
     testGame.rollDice();
@@ -186,11 +105,7 @@ describe("Qwixx Logic tests", () => {
   ])(
     "active-player marking a number that doesn't equal the sum of a white dice and a %s dice should throw an error",
     (row, num) => {
-      // const testGame = new QwixxLogic(playersArrayMock, diceMock as Dice);
-
       jest.spyOn(player1Mock, "submissionCount", "get").mockReturnValue(1);
-      // (player1Mock.markNumber as jest.Mock).mockReturnValue(true);
-      // player1Mock.markNumber.mockReturnValue(true);
       const testGame = new QwixxLogic(playersArrayMock, fakeDice);
 
       testGame.rollDice();
