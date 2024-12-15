aa5118cbb3198f59170c646a483085574831b713
kawrou <121672581+kawrou@users.noreply.github.com>
2024-12-15 23:16:49 +0000

    test:CellLockButton new test cases

--- client/tests/components/GameCard/CellLockButton.unit.test.tsx
+++ client/tests/components/GameCard/CellLockButton.unit.test.tsx
@@ -1,5 +1,5 @@
-import { describe, expect, test, vi } from "vitest"; // it - add later
-import { render, screen, waitFor } from "@testing-library/react"; // waitFor, within add later
+import { describe, expect, test, vi } from "vitest";
+import { render, screen } from "@testing-library/react";
 //import { userEvent } from "@testing-library/user-event";
 import React from "react";
 import { LockButton } from "../../../src/components/GameCard/CellLockButton";
@@ -9,17 +9,65 @@ import { RowColour } from "../../../src/types/enums";
 
 
 describe("Lock Button", () => {
-  test("button is disabled when locked", () => {
-    render(
-      <LockButton
-        colour={RowColour.Red}
-        isOpponent={false}
-        handleLockRow={vi.fn}
-        isLocked={true}
-      />
-    );
+  describe("Player:", () => {
+    test("button is disabled when locked", () => {
+      render(
+        <LockButton
+          colour={RowColour.Red}
+          isOpponent={false}
+          handleLockRow={vi.fn}
+          isLocked={true}
+        />
+      );
 
-    const btn = screen.getByRole("button");
-    expect(btn).toHaveAttribute("disabled")
-  });
+      const btn = screen.getByRole("button");
+      expect(btn).toHaveAttribute("disabled")
+    });
+
+    test("button is enabled when unlocked", () => {
+      render(
+        <LockButton
+          colour={RowColour.Red}
+          isOpponent={false}
+          handleLockRow={vi.fn}
+          isLocked={false}
+        />
+      );
+
+      const btn = screen.getByRole("button");
+      expect(btn).not.toHaveAttribute("disabled")
+    })
+  })
+
+  describe("Opponent:", () => {
+    test("renders a span that doesn't have CSS class 'locked' when row isn't locked", () => {
+      render(
+        <LockButton
+          colour={RowColour.Red}
+          isOpponent={true}
+          handleLockRow={vi.fn}
+          isLocked={false}
+        />
+      );
+
+      const btn = screen.getByLabelText("non-interactive-button");
+      expect(btn).toBeVisible()
+      expect(btn).not.toHaveClass("locked")
+    })
+
+    test("renders a span that has CSS class 'locked' when row is locked", () => {
+      render(
+        <LockButton
+          colour={RowColour.Red}
+          isOpponent={true}
+          handleLockRow={vi.fn}
+          isLocked={true}
+        />
+      );
+
+      const btn = screen.getByLabelText("non-interactive-button");
+      expect(btn).toBeVisible()
+      expect(btn).toHaveClass("locked")
+    });
+  })
 });
