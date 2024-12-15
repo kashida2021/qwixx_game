961a756da40f8559e87204c829f6b48e8b359d8f
kawrou <121672581+kawrou@users.noreply.github.com>
2024-12-15 23:16:11 +0000

    misc:Added notes to row and child button components

--- client/src/components/GameCard/RowCell.tsx
+++ client/src/components/GameCard/RowCell.tsx
@@ -1,30 +1,31 @@
 import React from 'react';
 
-interface NumberCellProps{
-    type:'number';
+// ISSUE: Seems like this component isn't actually used
+interface NumberCellProps {
+    type: 'number';
     num: number;
     numIndex: number;
     rowIndex: number;
     color: string;
 }
 
-interface LockedCellProps{
+interface LockedCellProps {
     type: 'lock';
     color: string;
 }
 
 type CellProps = NumberCellProps | LockedCellProps;
 
-const RowCell:React.FC<CellProps> = (props) => {
-    if(props.type === "lock"){
-            return <li>
-                    <button className={`lock-btn ${props.color}`}>🔒</button>
-                </li>
-        }
-    
+const RowCell: React.FC<CellProps> = (props) => {
+    if (props.type === "lock") {
+        return <li>
+            <button className={`lock-btn ${props.color}`}>🔒</button>
+        </li>
+    }
+
     return (
         <li>
-            <button className = {`cell-btn ${props.color}`}> 
+            <button className={`cell-btn ${props.color}`}>
                 {props.num}
             </button>
         </li>
