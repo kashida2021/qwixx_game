12d9273b83f16a4df3e3eefa90acf6825e3d3a57
kawrou <121672581+kawrou@users.noreply.github.com>
2024-12-16 18:33:12 +0000

    feat:Lock button state is reflected correctly by the UI

---
+++ client/tests/components/GameCard/__fixtures__/gameCardStates.ts
@@ -0,0 +1,98 @@
+import { QwixxLogic } from "../../../../src/types/qwixxLogic";
+import { GameCardData } from "../../../../src/types/GameCardData";
+
+export const gameCardBaseState: GameCardData = {
+	rows: {
+		red: [],
+		yellow: [],
+		green: [],
+		blue: [],
+	},
+	isLocked: {
+		red: false,
+		yellow: false,
+		green: false,
+		blue: false,
+	},
+	penalties: [],
+}
+
+export const gameCardMarkedState: GameCardData = {
+	rows: {
+		red: [6],
+		yellow: [],
+		green: [],
+		blue: [],
+	},
+	isLocked: {
+		red: false,
+		yellow: false,
+		green: false,
+		blue: false,
+	},
+	penalties: [],
+};
+
+export const gameCardTwoMarkedNumbersState: GameCardData = {
+	rows: {
+		red: [6, 9],
+		yellow: [],
+		green: [],
+		blue: [],
+	},
+	isLocked: {
+		red: false,
+		yellow: false,
+		green: false,
+		blue: false,
+	},
+	penalties: [],
+};
+
+export const gameCardLockRowConditionSatisfiedState: GameCardData = {
+	rows: {
+		red: [2, 3, 4, 5, 6, 12],
+		yellow: [],
+		green: [],
+		blue: [],
+	},
+	isLocked: {
+		red: false,
+		yellow: false,
+		green: false,
+		blue: false,
+	},
+	penalties: [],
+};
+
+export const gameCardPlayerLockedRowState: GameCardData = {
+	rows: {
+		red: [2, 3, 4, 5, 6, 12, 13],
+		yellow: [],
+		green: [],
+		blue: [],
+	},
+	isLocked: {
+		red: true,
+		yellow: false,
+		green: false,
+		blue: false,
+	},
+	penalties: [],
+};
+
+export const gameCardOpponentLockedRowState: GameCardData = {
+	rows: {
+		red: [2, 3, 4, 5, 6, 12],
+		yellow: [],
+		green: [],
+		blue: [],
+	},
+	isLocked: {
+		red: true,
+		yellow: false,
+		green: false,
+		blue: false,
+	},
+	penalties: [],
+};
