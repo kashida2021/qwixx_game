85b87ff18545c2dffb09db367e2415fc3a19b5f2
kawrou <121672581+kawrou@users.noreply.github.com>
2024-12-29 16:05:03 +0000

    ref:Use IDie interface to create mocks for QwixxLogic tests

---
+++ server/src/tests/services/__mocks__/QwixxLogicTestMocks.ts
@@ -0,0 +1,24 @@
+import IDie from "../../../models/IDie";
+import { DiceColour } from "../../../enums/DiceColours";
+
+function createDieMock(dieValue: number): IDie{
+    return {
+        get value(){
+            return dieValue
+        },
+        get active(){
+            return true;
+        },
+        rollDie: jest.fn().mockReturnValue(dieValue),
+        disable: jest.fn(),
+    }
+}
+
+export const mockSixSidedDice = {
+  [DiceColour.White1]: createDieMock(2),
+  [DiceColour.White2]: createDieMock(3),
+  [DiceColour.Red]: createDieMock(4),
+  [DiceColour.Yellow]: createDieMock(5),
+  [DiceColour.Green]: createDieMock(6),
+  [DiceColour.Blue]: createDieMock(1),
+};
