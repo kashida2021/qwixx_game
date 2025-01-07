059738f4aba6ce3c9cabb509cd7af3c6a57a63a6
Ken Ashida <kenashida@hotmail.com>
2025-01-07 16:57:09 +0000

    feat/added props to GamePage test to ensure all props are passed correctly. Added test for rendering EndGameModel but it doesnt fully work currently

---
+++ client/tests/pages/GamePage/__fixtures__/gameSummaryStates.ts
@@ -0,0 +1,20 @@
+export const endGameSummaryLockedState = {
+  winners: ["testuser1"],
+  scores: {
+    testuser1: {
+      subtotal: { red: 21, yellow: 3, green: 21, blue: 1 },
+      penalties: 0,
+      total: 46,
+    },
+    testuser2: {
+      subtotal: { red: 0, yellow: 0, green: 0, blue: 0 },
+      penalties: 0,
+      total: 0,
+    },
+    testuser3: {
+      subtotal: { red: 0, yellow: 0, green: 0, blue: 0 },
+      penalties: 0,
+      total: 0,
+    },
+  },
+};
