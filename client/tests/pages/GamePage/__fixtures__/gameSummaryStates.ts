5bc5b1065ac9d8d71207590f4448660f4087ca9a
Ken Ashida <kenashida@hotmail.com>
2025-01-12 10:33:15 +0000

    fix/refactored gameEndSummary mock to reflect correct data structure to pass test

--- client/tests/pages/GamePage/__fixtures__/gameSummaryStates.ts
+++ client/tests/pages/GamePage/__fixtures__/gameSummaryStates.ts
@@ -1,20 +1,23 @@
 export const endGameSummaryLockedState = {
   winners: ["testuser1"],
-  scores: {
-    testuser1: {
+  scores: [
+    {
+      name: "testuser1",
       subtotal: { red: 21, yellow: 3, green: 21, blue: 1 },
       penalties: 0,
       total: 46,
     },
-    testuser2: {
+    {
+      name: "testuser2",
       subtotal: { red: 0, yellow: 0, green: 0, blue: 0 },
       penalties: 0,
       total: 0,
     },
-    testuser3: {
+    {
+      name: "testuser3",
       subtotal: { red: 0, yellow: 0, green: 0, blue: 0 },
       penalties: 0,
       total: 0,
     },
-  },
+  ],
 };
