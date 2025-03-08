659dab20e421869b093b3ba89047ccd1ceca96ae
Ken Ashida <kenashida@hotmail.com>
2025-03-08 18:42:47 +0000

    task/removed tsc from package.json and added dist directory to build

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,10 +3,11 @@ import react from "@vitejs/plugin-react-swc";
 
 // https://vitejs.dev/config/
 export default defineConfig({
- plugins: [react()],
- test: {
-  environment: "jsdom",
-  globals: true,
-  setupFiles: "./tests/setup.ts",
- },
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    globals: true,
+    setupFiles: "./tests/setup.ts",
+  },
+  build: { outDir: "dist" },
 });
